6025a88240a1d70fd7d333e069b4479c4b0751e4
LiKTOi1029 <testingboy5689@gmail.com>
2026-08-14 13:27:34 +0330

    commands/setfile.lua adding full setfile functionality, preparing for testing

--- commands/setfile.lua
+++ commands/setfile.lua
@@ -1,8 +1,13 @@
 local Defaults = require("defaults")
 local Command = {}
 function Command.Execute(FileName)
-  if love.filesystem.getInfo(Defaults.SelectedFile) then
+  local File = io.open(FileName, "r")
+  if not File then
+	return Defaults.Errors.SelectedFileNotFound
+  else
 	Defaults.SelectedFile = FileName
+	File:close()
   end
+  return Defaults.Normal.SelectedTheFile
 end
 return Command
