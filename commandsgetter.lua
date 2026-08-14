24ae9864abdd8e71699dfb922aa75793f1938823
LiKTOi1029 <testingboy5689@gmail.com>
2026-08-14 12:08:47 +0330

    commandsgetter.lua implemented

--- commandsgetter.lua
+++ commandsgetter.lua
@@ -1,6 +1,11 @@
 local Getter = {}
 function Getter.Execute()
-  local AllCommands = love.filesystem.getDirectoryItems(Defaults.CommandsDir)
-  
+  local CookedCommands = {}
+  local RawCommands = love.filesystem.getDirectoryItems(Defaults.CommandsDir)
+  for Index, File in ipairs(RawCommands) do
+	CookedCommands[Index] = File:gsub(".lua", ""):upper()
+	RawCommands[Index] = Defaults.CommandsDir..File
+  end
+  return {CommandNames = CookedCommands, CommandPaths = RawCommands,}
 end
 return Getter
