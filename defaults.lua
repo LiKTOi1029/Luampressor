d6e09788c7bd86f8bfe0bdca85204786fb15a8e1
LiKTOi1029 <testingboy5689@gmail.com>
2026-08-14 13:10:45 +0330

    defaults.lua adding new defaults

--- defaults.lua
+++ defaults.lua
@@ -1,8 +1,15 @@
 local Defaults =
 {
+  WarningPrompt = "/> ",
+  ErrorPrompt = "X> ",
+  NormalPrompt = " > ",
   CommandsDir = "commands/",
   SelectedFile = false, -- false means no file is selected. String if a file is selected.
   CommandsList = {}, -- Intentionally empty, is filled out by commandgetter.lua
+}
+Defaults.Errors = 
+{
+  CommandNotFound = Defaults.ErrorPrompt .. "Command was not found! Please use the help command for commandlist\n",
 }
 
 
