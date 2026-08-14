e2b44a8ab3e38fedf2f4c4ef918b755fca72ccc5
LiKTOi1029 <testingboy5689@gmail.com>
2026-08-14 13:06:31 +0330

    defaults.lua adding comma

--- defaults.lua
+++ defaults.lua
@@ -2,7 +2,7 @@ local Defaults =
 {
   CommandsDir = "commands/",
   SelectedFile = false, -- false means no file is selected. String if a file is selected.
-  CommandsList = {} -- Intentionally empty, is filled out by commandgetter.lua
+  CommandsList = {}, -- Intentionally empty, is filled out by commandgetter.lua
 }
 
 
