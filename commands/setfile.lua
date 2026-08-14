3933e202bed41398f369f605b28e39335a7a520e
LiKTOi1029 <testingboy5689@gmail.com>
2026-08-14 12:00:29 +0330

    commands/setfile.lua implementing

--- commands/setfile.lua
+++ commands/setfile.lua
@@ -1,6 +1,8 @@
 local Defaults = require("Defaults")
 local Command = {}
 function Command.Execute(FileName)
-  
+  if love.filesystem.getInfo(Defaults.CommandsDir..Defaults.SelectedFile) then
+	Defaults.SelectedFile = FileName
+  end
 end
 return Command
