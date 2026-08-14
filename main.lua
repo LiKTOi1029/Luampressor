464ea59f4aae3710a644061ec95a8d10764eef0b
LiKTOi1029 <testingboy5689@gmail.com>
2026-08-14 13:41:14 +0330

    main.lua bugfixed exit command not working

--- main.lua
+++ main.lua
@@ -5,7 +5,7 @@ local InputProcessor = require("inputprocessor")
 function love.update(DeltaTime)
   io.write(Defaults.NormalPrompt)
   local Choice = InputProcessor.Execute("normal", io.read("*line"):upper())
-  if Choice == "EXIT" then love.event.quit()
+  if Choice[1] == "EXIT" then love.event.quit()
   elseif Defaults.CommandsList.CommandNames[Choice[1]] then
 	io.write(Defaults.CommandsList.CommandNames[Choice[1]].Execute(Choice), "\n")
   else io.write(Defaults.Errors.CommandNotFound) end
