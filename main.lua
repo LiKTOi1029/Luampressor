4f3f608f21253c6c6ce4f12e78125fdf79a386a5
LiKTOi1029 <testingboy5689@gmail.com>
2026-08-14 13:12:19 +0330

    main.lua implementing defaults.lua

--- main.lua
+++ main.lua
@@ -1,8 +1,10 @@
 local Defaults = require("defaults")
-local Commands = require("commandsgetter.lua"):Execute()
+local Commands = require("commandsgetter"):Execute()
 function love.update(DeltaTime)
-  io.write(" > ")
+  io.write(Defaults.NormalPrompt)
   local Choice = io.read("*line"):upper()
   if Choice == "EXIT" then love.event.quit()
-  elseif Choice end
+  elseif Defaults.CommandsList.CommandNames[Choice] then
+	io.write(Defaults.NormalPrompt, Defaults.CommandList.CommandNames[Choice](Choice), "\n")
+  else io.write(Defaults.Errors.CommandNotFound) end
 end
