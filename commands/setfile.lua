94d9ac5b83e71837f6ac62ae8d21404631359f41
LiKTOi1029 <testingboy5689@gmail.com>
2026-08-14 13:42:14 +0330

    commands/setfile.lua adding new debugging

--- commands/setfile.lua
+++ commands/setfile.lua
@@ -2,6 +2,12 @@ local Defaults = require("defaults")
 local Command = {}
 function Command.Execute(Choice)
   local FileName = Choice[2]
+  if DevMode then io.write(Defaults.Dev.SetFile.PeekElements)
+    for i, v in ipairs(Choice) do
+	  io.write(Defaults.DevPrompt, v, "\n")
+	end
+	io.write(Defaults.Dev.SetFile.ElementsPeeked)
+  end
   local File = io.open(FileName, "r")
   if not File then
 	return Defaults.Errors.SelectedFileNotFound
