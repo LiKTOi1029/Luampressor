f6663d87423ef74766dd74e1fb88fb552f52e6a3
LiKTOi1029 <testingboy5689@gmail.com>
2026-08-14 13:48:25 +0330

    main.lua implementing debugging

--- main.lua
+++ main.lua
@@ -5,6 +5,13 @@ local InputProcessor = require("inputprocessor")
 function love.update(DeltaTime)
   io.write(Defaults.NormalPrompt)
   local Choice = InputProcessor.Execute("normal", io.read("*line"):upper())
+  
+  if Defaults.DevMode then io.write(Defaults.Dev.Main.PeekingProcessedInput)
+	for i, v in ipairs(Choice) do
+	  io.write(Defaults.DevPrompt, v, "\n")
+	end
+	io.write(Defaults.Dev.Main.ProcessedInputPeeked)
+  end
   if Choice[1] == "EXIT" then love.event.quit()
   elseif Defaults.CommandsList.CommandNames[Choice[1]] then
 	io.write(Defaults.CommandsList.CommandNames[Choice[1]].Execute(Choice), "\n")
