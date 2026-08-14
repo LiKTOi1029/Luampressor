a96f999770ad2ffa21b0b47b5f1178a7dd993d16
LiKTOi1029 <testingboy5689@gmail.com>
2026-08-14 13:42:55 +0330

    commands/setfile.lua bugfixing conditional of debug text

--- commands/setfile.lua
+++ commands/setfile.lua
@@ -2,7 +2,7 @@ local Defaults = require("defaults")
 local Command = {}
 function Command.Execute(Choice)
   local FileName = Choice[2]
-  if DevMode then io.write(Defaults.Dev.SetFile.PeekElements)
+  if Defaults.DevMode then io.write(Defaults.Dev.SetFile.PeekElements)
     for i, v in ipairs(Choice) do
 	  io.write(Defaults.DevPrompt, v, "\n")
 	end
