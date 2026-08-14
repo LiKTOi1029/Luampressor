e38d4641edc7ba1c0260afd27fe44fd30762e221
LiKTOi1029 <testingboy5689@gmail.com>
2026-08-14 17:06:54 +0330

    defaults.lua adding new default for output file names

--- defaults.lua
+++ defaults.lua
@@ -5,6 +5,7 @@ local Defaults =
   NormalPrompt = " > ",
   CommandsDir = "commands/",
   SelectedFile = false, -- false means no file is selected. String if a file is selected.
+  OutputName = "Output",
   CommandsList = {}, -- Intentionally empty, is filled out by commandgetter.lua
   DevMode = true,
   DevPrompt = "D> ",
