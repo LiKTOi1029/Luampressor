5f23ce509c80dede15c97840300857d1d0f0a676
LiKTOi1029 <testingboy5689@gmail.com>
2026-08-14 13:20:55 +0330

    defaults.lua adding new DevMode

--- defaults.lua
+++ defaults.lua
@@ -6,10 +6,12 @@ local Defaults =
   CommandsDir = "commands/",
   SelectedFile = false, -- false means no file is selected. String if a file is selected.
   CommandsList = {}, -- Intentionally empty, is filled out by commandgetter.lua
+  DevMode = true,
 }
 Defaults.Errors = 
 {
   CommandNotFound = Defaults.ErrorPrompt .. "Command was not found! Please use the help command for commandlist\n",
+  SelectedFileNotFound = Defaults.ErrorPrompt .. "No files are selected"
 }
 
 
