08fabd837cc0dbd192068d45fdd40e32e78f9107
LiKTOi1029 <testingboy5689@gmail.com>
2026-08-15 12:25:40 +0330

    commands/compress.lua implementing defaults.lua

--- commands/compress.lua
+++ commands/compress.lua
@@ -6,10 +6,15 @@ function StandardCompression(Input)
   local FileExtension = Defaults.SelectedFile:sub(Defaults.SelectedFile:find("%.%a+$"))
   local FileOutput = Defaults.SelectedFile:gsub("%.%a+$", "") .. Defaults.OutputName .. Date .. FileExtension
   local File = io.open(Defaults.SelectedFile, "r")
+  local Data = File:read("*all")
+  if Data and Defaults.DevMode then io.write(Defaults.Dev.Compress.SelectedFileExtracted) end
+  if not Data then io.write(Defaults.Errors.Compress) end
+  File:close()
+  
   return Defaults.Normal.CompressedTheFile
 end
 function InlineCompression(Input)
-
+  return Defaults.Normal.NotImplemented
 end
 function Compress.Execute(Input)
   if #Input > 1 then
