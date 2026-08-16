159b52e869b39325ad38e3ecfc0ea3a5179a1fca
LiKTOi1029 <testingboy5689@gmail.com>
2026-08-16 18:29:34 +0330

    inputprocessor.lua filling out brand new function for lossless compression tablification

--- inputprocessor.lua
+++ inputprocessor.lua
@@ -20,14 +20,29 @@ local Types =
 	if Defaults.DevMode then io.write(Defaults.Dev.InputProcessor.EndOfNormalType) end
 	return ResultingTable
   end,
-  ["prepare_for_compression"] = function(Input)
+  ["prepare_for_compression_lossless"] = function(Input)
 	local ResultingTable = {}
+	local WordCount = {}
 	local ParsingString = ""
 	local Len = Input:len()
-	for Index = Input, Len, 1 do
+	for Index = 1, Len, 1 do
 	  local Sub = Input:sub(Index, Index)
-	  
+	  if i == Len then
+		ParsingString = ParsingString .. Sub
+		ResultingTable[#ResultingTable+1] = ParsingString
+		if not WordCount[ParsingString] then WordCount[ParsingString] = 1
+		else WordCount[ParsingString] = WordCount[ParsingString] + 1 end
+		ParsingString = nil
+	  elseif Sub == " " then
+	    ResultingTable[#ResultingTable+1] = ParsingString
+		if not WordCount[ParsingString] then WordCount[ParsingString] = 1
+		else WordCount[ParsingString] = WordCount[ParsingString] + 1 end
+		ParsingString = ""
+	  else
+	    ParsingString = ParsingString .. Sub
+	  end
 	end
+	return {WordCount = WordCount, ResultingTable = ResultingTable}
   end
 }
 
